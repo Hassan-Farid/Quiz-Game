94566bf777c2f6d1e8ee60a343bc9600e699740b
Hassan Farid <hassan.far.661@gmail.com>
2019-09-16 23:16:21 +0500

    Added game music

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 import tkinter as tk
 import rungame as rg
-from winsound import *
-
-def playsound():
-    PlaySound('music.wav', SND_FILENAME|SND_LOOP|SND_ASYNC)
 
 def main():
     gameWin = tk.Tk()
     gameWin.title("Quiz Game (Application) ")
     gameWin.configure(bg = "purple")
     gameWin.wm_state("zoomed")
-    playsound()
     runApp = rg.maingame(gameWin)
     gameWin.mainloop()
 
--- rungame.py
+++ rungame.py
@@ -2,11 +2,15 @@ import tkinter as tk
 import startgame as sg
 import instructgame as ig
 import message as msg
+import sys
+from winsound import *
 
 class maingame():
 
     def __init__(self, master):
         self.master = master
+
+        self.playsound()
 
         self.maingameLabel = tk.Label(self.master, bg='black', fg='yellow', text='QUIZ GAME',  font='Times 48 bold italic', borderwidth=20, relief=tk.RAISED)
         self.maingameLabel.place(x=400, y=40, width=500)
@@ -20,6 +24,9 @@ class maingame():
         self.exitBtn = tk.Button(self.master, text='EXIT', bg='black', fg='yellow', font='Times 16 bold italic', borderwidth=8, command = self.confirm)
         self.exitBtn.place(x=500, y=400, width=300)
 
+    
+    def playsound(self):
+        PlaySound('music.wav', SND_FILENAME|SND_LOOP|SND_ASYNC)
 
     def runstart(self):
         start = sg.start(self.master)
@@ -28,7 +35,7 @@ class maingame():
         instruct = ig.instruct(self.master)
 
     def confirm(self):
-        confirm_mes = msg.Message(self.master, "Are you sure you want to quit?")
+        msg.Message(self.master, "Are you sure you want to quit?")
         
 
 
